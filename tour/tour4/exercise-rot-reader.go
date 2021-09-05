69731956746c970131bc06bb9350551d634ebf73
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-09-05 12:43:34 +0900

    exercise: rot13reader

---
+++ tour/tour4/exercise-rot-reader.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"io"
+	"os"
+	"strings"
+)
+
+type rot13Reader struct {
+	r io.Reader
+}
+
+func (reader rot13Reader) Read(rb []byte) (n int, e error) {
+	n, e = reader.r.Read(rb)
+	for i := range rb {
+		rb[i] = lot13(rb[i])
+	}
+	return
+}
+
+func lot13(b byte) byte {
+	if ('A' <= b && b <= 'L') || ('a' <= b && b <= 'l') {
+		return b + 13
+	}
+	return b - 13
+}
+
+func main() {
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
+	r := rot13Reader{s}
+	io.Copy(os.Stdout, &r)
+}
